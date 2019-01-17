26b19cc9294073d06aab9b656a8925c616049daf
Chris Tokita <christopher.tokita@gmail.com>
2019-01-17 17:03:22 -0500

    Finish processing data

--- state_diversity/scripts/1_processing data.R
+++ state_diversity/scripts/1_processing data.R
@@ -19,3 +19,8 @@ state_data <- read.csv("state_diversity/data/State_Demographic_Data.csv", as.is 
 state_perc <- state_data %>% 
   gather(key = Group, value = Percent, -State, -Total_Pop) %>% 
   mutate(Percent = Percent / Total_Pop)
+
+###############
+# Save
+###############
+save(state_perc, file = "state_diversity/data/State_Demographic_Percents.Rdata")
